1fb64dd7ddad248d5249b035db6c04e67f75972c
Modory <dashkarni@gmail.com>
2025-11-05 21:34:44 +0300

    yeye

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+<<<<<<< HEAD
   const apiUrl = env.VITE_API_URL || 'http://localhost:4000/'
+=======
+  const apiUrl = env.VITE_API_URL || 'http://localhost:3000/'
+>>>>>>> 7dc5961f667e017a22553f1303a380e973f90311
 
   const secure = apiUrl.startsWith('https://')
 
@@ -17,7 +21,18 @@ export default ({ mode }) => {
           changeOrigin: true,
           secure,
         },
+<<<<<<< HEAD
       },
     },
+=======
+        '/uploads': {
+          target: apiUrl,
+          changeOrigin: true,
+          secure,
+        },
+      },
+    },
+
+>>>>>>> 7dc5961f667e017a22553f1303a380e973f90311
   })
 }
