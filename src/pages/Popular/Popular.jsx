c49377a433c7436e6a8424a8a4f948e537d50c64
Kin Kate <kinkatusha@gmail.com>
2025-12-20 22:02:10 +0100

    fix: херня с лайками

--- src/pages/Popular/Popular.jsx
+++ src/pages/Popular/Popular.jsx
@@ -1,15 +1,15 @@
 import styles from "./Popular.module.scss"
 import BlocksRecipe from '../../components/BlocksRecipe/BlocksRecipe';
 import { useTranslation } from "react-i18next";
-import { useState, useEffect } from "react";
+import {useState, useEffect, useContext} from "react";
 const POPULAR_URL = "/api/v1/recipes/popular";
 import axios from "axios";
+import AuthContext from "../../context/AuthProvider.jsx";
 
 const Popular = () => {
   const {t} = useTranslation();
 
-  //const {auth} = useContext(AuthContext);
-  //const token = auth?.token;
+  const {auth} = useContext(AuthContext);
   const [popularData, setPopularData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -20,10 +20,12 @@ const Popular = () => {
       try{
         setError(null);
         setIsLoading(true);
+        const headers = {};
+        if(auth.token){
+          headers.Authorization = `Bearer ${auth.token}`;
+        }
         const response = await axios.get(POPULAR_URL, {
-          headers: {
-            //'Authorization': `Bearer ${token}`
-          }
+          headers
         });
         console.log('API response:', response.data);
         const popularArray = response.data.data;
